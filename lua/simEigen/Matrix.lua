f5eb7dd2e4a40e639e9ec2ad44011c14796e7763
Coppelia <mfreese@gmx.ch>
2026-05-18 17:55:02 +0200

    figure out other matrix dim if one is omitted

--- lua/simEigen/Matrix.lua
+++ lua/simEigen/Matrix.lua
@@ -924,9 +924,12 @@ function Matrix:tomatrix(v, rows, cols)
             return Matrix(rows, cols, v)
         elseif rows == nil and cols == nil then
             return Matrix:fromtable(v)
-        else
-            error 'invalid args'
+        elseif rows and (#v % rows == 0) then
+            return Matrix(rows, #v / rows, v)
+        elseif cols and (#v % cols == 0) then
+            return Matrix(#v // cols, cols, v)
         end
+        error 'invalid args'
     end
     error 'invalid data'
 end
