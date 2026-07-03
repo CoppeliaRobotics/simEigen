92816bac6b28fe9b3e96943143083adc7558d525
Federico Ferri <federico.ferri.it@gmail.com>
2026-07-03 15:16:39 +0200

    fix 'expected int' err

--- lua/simEigen/Matrix.lua
+++ lua/simEigen/Matrix.lua
@@ -40,7 +40,7 @@ function Matrix:initialize(rows, cols, data, order)
     if data == nil or type(data) == 'number' then
         assert(rows >= 0, 'rows must be non-negative')
         assert(cols >= 0, 'cols must be non-negative')
-        self.__handle = simEigen.mtxNew(rows, cols, nil, nil, data)
+        self.__handle = simEigen.mtxNew(rows, cols, nil, 0, data)
     else
         assert(rows == -1 or rows >= 0, 'rows must be non-negative, or -1')
         assert(cols == -1 or cols >= 0, 'cols must be non-negative, or -1')
